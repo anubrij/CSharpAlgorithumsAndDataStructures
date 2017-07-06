61512a626acd7f6d4e1673d60d25baa01a45b69f
ANUBRIJ\Anubrij <anubrij@live.com>
2017-07-06 11:22:41 +0530

    First Commit

---
+++ Project/Project/Array/ArraySolution.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectDS.Array
+{
+   public class ArraySolution
+    {
+        /// <summary>
+        /// @Proble Statement : Implement an algorithm to determine if a string has all unique characters. What if you
+        /// cannot use additional data structures?
+        /// @Constraint :
+        /// 1 <= s <= 10^3
+        /// @Input:
+        /// single string "S"
+        /// @output:
+        /// True or Flase
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static bool IsUnique(string s) {
+            for (int i = 0; i < s.Length; i++) {
+                if (i != s.LastIndexOf(s[i]))
+                    return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ Project/ProjectTest/ProjectTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace ProjectTest
+{
+    [TestFixture]
+    public class ProjectTest
+    {
+        [TestCase]
+        public void TestArray()
+        {
+
+            Assert.AreEqual(ProjectDS.Array.ArraySolution.IsUnique("anubrijchandra"), false);
+            Assert.AreEqual(ProjectDS.Array.ArraySolution.IsUnique("qwertyasdfghjzxcvbnm"), true);
+        }
+    }
+}
